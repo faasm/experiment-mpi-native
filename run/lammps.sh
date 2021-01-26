4c699990f39f7ce7ff09023a74807d4bc3571019
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-01-26 07:20:31 +0000

    wip

--- run/lammps.sh
+++ run/lammps.sh
@@ -4,7 +4,7 @@ set -e
 
 THIS_DIR=$(dirname $(readlink -f $0))
 PROJ_ROOT=${THIS_DIR}/..
-LAMMPS_DIR=${THIS_DIR}/lammps
+LAMMPS_DIR=${PROJ_ROOT}/lammps
 LAMMPS_BUILD_DIR=${LAMMPS_DIR}/build
 
 export LD_LIBRARY_PATH=/build/faabric/install/lib
